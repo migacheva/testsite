25e1b5e46aea36a76c5f0c985377240d5f27e7e1
migacheva <migacheva9>
2020-06-30 10:45:02 +0300

    Урок 30.06.2020 Динамические страницы

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-id')
+
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request=request, template_name='news/index.html', context=context)
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
