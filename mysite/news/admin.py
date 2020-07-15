d20f1bfb4270fcda9113559441d30b565d853870
migacheva <migacheva9>
2020-07-15 10:17:10 +0300

    Урок 10 от 15.07.2020 (часть 2)

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -7,7 +7,7 @@ class NewsAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
-    list_filter = ('is_published', 'category')
+    list_filter = ('is_published', 'category', 'created_at')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
