af90e2cf63e296dc352ea52e4c94baba0822925c
migacheva <migacheva9>
2020-07-30 15:55:07 +0300

    Урок 11 от 29.07.2020

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,6 +10,11 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+
+    def my_func(self):
+        return "bla bla function"
+
+
     def __str__(self):
         return self.title
 
